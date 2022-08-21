d66f9d59cb4661cb0c5f8c226e61cdd82c5c7398
muryshevamarina <marilz@bk.ru>
2022-08-22 01:18:21 +0500

    error

---
+++ task_4/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+Console.WriteLine("Введите число");
+int numberN = Convert.ToInt32(Console.ReadLine());
+int numberM = 2;
+Console.Clear();
+
+if (numberM<=numberN)
+
+{
+    Console.WriteLine(numberM+2);
+
+}
+
