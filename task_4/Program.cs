14e735d7782ef1f95c939c941821d9b3f18c9218
Marina <110925094+MarinaMurysheva@users.noreply.github.com>
2022-08-27 13:31:23 +0500

    Update Program.cs
    fixed error

--- task_4/Program.cs
+++ task_4/Program.cs
@@ -4,10 +4,10 @@ int numberN = Convert.ToInt32(Console.ReadLine());
 int numberM = 2;
 Console.Clear();
 
-if (numberM<=numberN)
+while (numberM<=numberN)
 
 {
-    Console.WriteLine(numberM+2);
-
+    Console.WriteLine(numberM);
+    numberM+=2;
 }
 
